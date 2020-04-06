123b76aff8424d9bc4d703f8966dfc8a6f0ace89
kamronald <35947132+kamronald@users.noreply.github.com>
2020-04-05 23:22:36 -0700

    Update reaction_propagator_new.py

--- pymatgen/reactions/reaction_propagator_new.py
+++ pymatgen/reactions/reaction_propagator_new.py
@@ -45,20 +45,47 @@ class ReactionPropagator:
 
     def __init__(self, reaction_network):
         self.reaction_network = reaction_network
+        self.reaction_list = reaction_network.reactions ### what to use as inputs for this function?
+        ### should be list of reaction objects
 
+        ### make a dict, assigning an index for each reaction. Each index is a dict containing reaction object and will later add propensity
+        self.reactions = {}
+        for id, reaction in enumerate(self.reaction_list):
+            self.reactions[id] = reaction
+        self.entries_dict = self.reaction_network.entries ### Dict containing all entries
 
     def reaction_propensities(self, state):
-        """ Obtain reaction propensities, based on reaction kinetics. Propensity = (rate)(orientation factor).
-            Loop through all reaction nodes, obtain rate constant
+        """ Obtain reaction propensities, based on reaction kinetics. Propensity = (rate)(# distinct reactant combinations).
+            Loop through all reactions, obtain rate constant
 
             Inputs:
-            self - need all reaction nodes
-            state (array) number of molecules of each species
+            state (dict?) molecule entries, along with concentration of each molecule?
 
             Returns:
-            (Num_rxns x 1) Array of reaction propensities
+            (Num_rxns x 1) Dictionary of reaction propensities
         """
-
+        propensities = {}
+        for id, reaction in enumerate(self.reactions):
+            rate = self.calculate_rate(self, state, temperature=298.0, reverse=False, kappa=1.0)
+            reactants = reaction.reactants
+            num_reactants = len(reactants)
+            concentrations = []
+            ## Find matching id, and get the concentration
+            for this_reactant in reactants:
+                reactant_id = this_reactant.entry_id
+                for x in state:
+                    if reactant_id == state[x]["Molecule"].entry_id: ## depends on data structure of "state"
+                        concentrations = np.append(concentrations, state[x]["Concentration"])
+                        break
+            if num_reactants == 1:
+                h = concentrations[0]
+            elif (num_reactants == 2) & (reactants[0] == reactants[1]):
+                h = concentrations[0] * (concentrations[0] - 1) / 2
+            elif (num_reactants == 2) & (reactants[0] != reactants[1]):
+                h = concentrations[0] * concentrations[1]
+            else:
+                raise RuntimeError("Only single and bimolecular reactions supported by this simulation")
+            propensities[id] = h * rate
         return propensities
 
 
@@ -79,8 +106,12 @@ class ReactionPropagator:
             current_state = state[-1,:]
             ## Obtain reaction propensities, on which the probability distributions of
             ## time and reaction choice depends.
-            propensities = self.reaction_propensities(self, current_state) ## should be an array of each reaction's propensity
-            a = np.sum(propensities)
+            propensities = self.reaction_propensities(self, current_state) ## dict of each reaction's propensity
+            a = 0
+            ## Loop to sum up all propensities
+            for i in propensities:
+                a = a + propensities[i]
+
             ## drawing random numbers on uniform (0,1) distrubution
             r1 = np.random.random_sample()
             r2 = np.random.random_sample()
@@ -92,12 +123,12 @@ class ReactionPropagator:
             ## Choosing a reaction mu; need a cumulative sum of rxn propensities
             ## Discrete probability distrubution of reaction choice
             prop_sum = 0
-            for i, ai in enumerate(propensities):
-                prop_sum = prop_sum + ai
+            for i in propensities:
+                prop_sum = prop_sum + propensities[i]
                 if prop_sum > r2*a:
                     mu = i
                     break
-
+            reaction_mu = self.reactions[mu]
             ## Next work on updating state based on choice of reaction
             ## relate rxn index mu to the actual reaction node, and update state
 

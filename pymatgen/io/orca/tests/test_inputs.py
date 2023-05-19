05688e8d205c1948b94f85ae344b470f63223615
Evan Spotte-Smith <espottesmith@gmail.com>
2023-05-19 16:56:40 -0700

    Tested; let's see if they pass

--- pymatgen/io/orca/tests/test_inputs.py
+++ pymatgen/io/orca/tests/test_inputs.py
@@ -28,15 +28,92 @@ def test_template():
              "string": "str",
              "integer": 1,
              "float": 1.337}
+    template = ORCAInput.template(block, "example")
+    assert template == """%example
+  array 1,2,3
+  string str
+  integer 1
+  float 1.337
+end
+"""
 
 def test_find_blocks():
-    pass
+    test_input = """! NORI CPCM Opt Freq ExtremeSCF
+
+* xyz -1 1
+ O      0.0000000000      0.0000000000      0.0000000000
+ H      0.0000000000      0.0000000000      0.9100000000
+*
+
+%basis
+  basis def2-SVPD
+end
+
+%cpcm
+  smd True
+  smdsolvent water
+end
+
+%geom
+  maxiter 100
+end
+
+%method
+  method wB97M-V
+end
+
+%pal
+  nprocs 2
+end"""
+
+    blocks = ORCAInput.find_blocks(test_input)
+    assert blocks == ["basis", "cpcm", "geom", "method", "pal"]
 
 def test_read_molecule():
-    pass
+    mol_block = """* xyz -1 1
+ O      0.0000000000      0.0000000000      0.0000000000
+ H      0.0000000000      0.0000000000      0.9100000000
+*"""
+
+    mol = ORCAInput.read_molecule(mol_block)
+    reference = Molecule(["O", "H"], [[0.0, 0.0, 0.0], [0.0, 0.0, 0.91]], charge=-1.0)
+    assert mol == reference
 
 def test_read_simple_input():
-    pass
+    si_example = "! NORI CPCM Opt Freq ExtremeSCF"
+
+    parsed = ORCAInput.read_simple_input(si_example)
+    assert parsed == ["NORI", "CPCM", "Opt", "Freq", "ExtremeSCF"]
 
 def test_read_block():
-    pass
+    test_input = """! NORI CPCM Opt Freq ExtremeSCF
+
+* xyz -1 1
+ O      0.0000000000      0.0000000000      0.0000000000
+ H      0.0000000000      0.0000000000      0.9100000000
+*
+
+%basis
+  basis def2-SVPD
+end
+
+%cpcm
+  smd True
+  smdsolvent water
+end
+
+%geom
+  maxiter 100
+end
+
+%method
+  method wB97M-V
+end
+
+%pal
+  nprocs 2
+end"""
+
+    cpcm_block = ORCAInput.read_block(test_input, "cpcm")
+    
+    assert cpcm_block == {"smd": "True", "smdsolvent": "water"}
